d67a83161fad8e46f69747479b9a40285acd0a09
jdalton <john.david.dalton@gmail.com>
2024-05-15 16:35:30 -0400

    test: add benchmark for filtering known elements

--- test/benchmark.spec.js
+++ test/benchmark.spec.js
@@ -2,10 +2,10 @@
 
 const { readXMLFile } = require('./testUtils')
 const { convertXML } = require('../lib/simpleXmlToJson.min.js')
+const xmlInput = readXMLFile(__dirname + '/benchmark-input.xml')
 
 describe('transpiler', () => {
-    it('Benchmarking the library', () => {
-        const xmlInput = readXMLFile(__dirname + '/benchmark-input.xml')
+    it('Benchmarking baseline', () => {
         const start = Date.now()
         const iterations = 4000
         for (let i = 0; i < iterations; i += 1) {
@@ -13,7 +13,22 @@ describe('transpiler', () => {
         }
         const end = Date.now()
         console.log(
-            `avg exec time of ${iterations} iterations (in ms): ${
+            `Benchmarking baseline:\navg exec time of ${iterations} iterations (in ms): ${
+                (end - start) / iterations
+            }`
+        )
+        expect(end - start).toBeGreaterThan(0)
+    })
+    it('Benchmarking filtering known elements', () => {
+        const start = Date.now()
+        const iterations = 4000
+        const options = { knownElement: (t) => t === 'root' || t === 'star' }
+        for (let i = 0; i < iterations; i += 1) {
+            convertXML(xmlInput, options)
+        }
+        const end = Date.now()
+        console.log(
+            `Benchmarking filtering known elements:\navg exec time of ${iterations} iterations (in ms): ${
                 (end - start) / iterations
             }`
         )
